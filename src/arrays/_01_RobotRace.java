b1152d9fe48464c79e07dc03407e7dc923488dd1
TheMasterOfIron <league@worthymini-5.attlocal.net>
2018-11-04 17:26:05 -0800

    robot race, find the hidden button ip

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -10,38 +10,41 @@ public class _01_RobotRace {
 		//2. create an array of 5 robots.
 
 		//3. use a for loop to initialize the robots.
-		Robot[] r = new Robot[5];
+		Robot[] r = new Robot[7];
 		for (int i = 0; i < r.length; i++) {
-			r[i] = new Robot();
-			r[i].setX(i*200);
-			r[i].setY(500);
+			r[i] = new Robot("mini");
+			r[i].setX(500);
+			r[i].setY(300);
+			//4. make each robot start at the bottom of the screen, side by side, facing up
+
+			//5. use another for loop to iterate through the array and make each robot move 
+			//   a random amount less than 50.
 		}
 		boolean ifwon = false;
 		while (ifwon == false) {
 			for (int i = 0; i < r.length; i++) {
-				Random random = new Random();
-				int rand = random.nextInt(50);
+				//Random random = new Random();
+				//int rand = random.nextInt(50);
 				r[i].setSpeed(20);
-				r[i].move(rand);
-				if(r[i].getY()<0) {
+				r[i].turn(-40);
+				r[i].move(100);
+				//r[i].move(rand);
+				if(r[i].getY()==300) {
+					//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+
+					//7. declare that robot the winner and throw it a party!	
 					ifwon = true;
 					System.out.println("winner!");
+					//8. try different races with different amounts of robots.
+
+					//9. make the robots race around a circular track.
 				}
 			}
+			
+			
 			
 		}
 	
 	}
-		//4. make each robot start at the bottom of the screen, side by side, facing up
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-		
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
-	//7. declare that robot the winner and throw it a party!
-
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
 }
